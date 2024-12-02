6e3f020caa0613051f1a1c8476e381fd7beeec50
Carmel Gafa <carmelgafa@gmail.com>
2024-12-02 21:38:52 +0100

    added different project parameters

--- backend/story_parameters.py
+++ backend/story_parameters.py
@@ -1,14 +1,14 @@
 # READER_AGE:int=15
 # BOOK_STYLE:str="Edgar Allan Poe"
 # BOOK_THEME:str="tension and a sense or horror"
+# MAXIMUM_PAGES:int=10
 
+READER_AGE:int=13
+BOOK_STYLE:str="Elisabetta Dami"
+BOOK_THEME:str="adventure and suspense"
+MAXIMUM_PAGES:int=5
 
-# READER_AGE:int=13
-# BOOK_STYLE:str="Elisabetta Dami"
-# BOOK_THEME:str="adventure and suspense"
-
-
-READER_AGE:int=4
-BOOK_STYLE:str="Lack Kent"
-BOOK_THEME:str="friendship and feel good"
-MAXIMUM_PAGES:int=4
+# READER_AGE:int=4
+# BOOK_STYLE:str="Lack Kent"
+# BOOK_THEME:str="friendship and feel good"
+# MAXIMUM_PAGES:int=4
